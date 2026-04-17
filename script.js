6b57d2d9b2755c94aaefa8ea42acc58ab6e70a29
abhinav05-bhatpude <abhinavbhatpude@gmail.com>
2026-04-16 19:21:01 -0700

    Added withdraw and checkBalance methods to Account class

--- script.js
+++ script.js
@@ -3,8 +3,21 @@ class Account{
         this.name=name;
         this.balance=balance;
     }
-    
+
     deposit(amount){
-        this.balance+=amount
+        if(amount>0){
+            this.balance+=amount;
+        }
+    }
+    withdraw(amount){
+        if(amount>this.balance){
+            console.log("Insuffecient Balance");
+        }
+        else{
+            this.balance-=amount;
+        }
+    }
+    checkBalance(){
+        console.log(`Balance: ${this.balance}`);
     }
 }
