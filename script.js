98161c37792c029762fa9b109be5211e5149da64
abhinav05-bhatpude <abhinavbhatpude@gmail.com>
2026-04-21 12:41:05 -0700

    Implemented account creation using user input and Account class with basic validation

--- script.js
+++ script.js
@@ -1,8 +1,77 @@
-const nameInput=document.getElementById("name");
-const amountInput=document.getElementById("amount");
-const createBtn=document.getElementById("createBtn");
-const depositBtn=document.getElementById("depositBtn");
-const withdrawBtn=document.getElementById("withdrawBtn");
-const showBtn=document.getElementById("showBtn");
-const output=document.getElementById("message");
+class Account{
+    static totalAccounts=0;
+    static bankName="MyBank";
+    constructor(name,balance){
+        this.name=name;
+        this.balance=balance;
+        this.accountNumber=Date.now();
 
+        Account.totalAccounts++;
+
+    }
+
+    deposit(amount){
+        if(amount>0){
+            this.balance+=amount;
+            console.log(`${this.name} deposited ${amount}`);
+        }
+    }
+    withdraw(amount){
+        if(amount>this.balance){
+            console.log("Insuffecient Balance");
+        }
+        else{
+            this.balance-=amount;
+        }
+    }
+    checkBalance(){
+        console.log(`Balance: ${this.balance}`);
+    }
+    getAccountInfo(){
+        console.log(`Name: ${this.name}`);
+        console.log(`balance: ${this.balance}`);
+        console.log(`Account No: ${this.accountNumber}`);
+    }
+
+    static showTotalAccounts(){
+        console.log(`Total Accounts : ${Account.totalAccounts}`);
+    }
+}
+
+class SavingsAccount extends Account{
+    constructor(name,balance,interestRate){
+        super(name,balance);
+        this.interestRate=interestRate;
+    }
+    addInterest(){
+        if(this.balance>0){
+            const interest=(this.balance * this.interestRate)/100;
+            this.balance+=interest;
+        }
+
+    }
+}
+
+const nameInput = document.getElementById("name");
+const amountInput = document.getElementById("amount");
+
+const createBtn = document.getElementById("createBtn");
+const depositBtn = document.getElementById("depositBtn");
+const withdrawBtn = document.getElementById("withdrawBtn");
+const showBtn = document.getElementById("showBtn");
+
+const output = document.getElementById("message");
+
+let user;
+createBtn.addEventListener("click",function(){
+    const name=nameInput.value.trim();
+    const amount=Number(amountInput.value);
+
+    if(!name || amount <=0){
+        output.innerText="Enter valid input ❌";
+        return;
+    }
+    user=new Account(name,amount);
+
+    output.innerText=`Account created for ${user.name} ✅`
+})
