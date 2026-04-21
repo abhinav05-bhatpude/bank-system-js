d7869c98c15066aff71c9ce16e3767a8f1f74437
abhinav05-bhatpude <abhinavbhatpude@gmail.com>
2026-04-21 12:27:25 -0700

    Connected Dom elements with Javascript

--- script.js
+++ script.js
@@ -1,65 +1,8 @@
-class Account{
-    static totalAccounts=0;
-    static bankName="MyBank";
-    constructor(name,balance){
-        this.name=name;
-        this.balance=balance;
-        this.accountNumber=Date.now();
+const nameInput=document.getElementById("name");
+const amountInput=document.getElementById("amount");
+const createBtn=document.getElementById("createBtn");
+const depositBtn=document.getElementById("depositBtn");
+const withdrawBtn=document.getElementById("withdrawBtn");
+const showBtn=document.getElementById("showBtn");
+const output=document.getElementById("message");
 
-        Account.totalAccounts++;
-
-    }
-
-    deposit(amount){
-        if(amount>0){
-            this.balance+=amount;
-            console.log(`${this.name} deposited ${amount}`);
-        }
-    }
-    withdraw(amount){
-        if(amount>this.balance){
-            console.log("Insuffecient Balance");
-        }
-        else{
-            this.balance-=amount;
-        }
-    }
-    checkBalance(){
-        console.log(`Balance: ${this.balance}`);
-    }
-    getAccountInfo(){
-        console.log(`Name: ${this.name}`);
-        console.log(`balance: ${this.balance}`);
-        console.log(`Account No: ${this.accountNumber}`);
-    }
-
-    static showTotalAccounts(){
-        console.log(`Total Accounts : ${Account.totalAccounts}`);
-    }
-}
-
-class SavingsAccount extends Account{
-    constructor(name,balance,interestRate){
-        super(name,balance);
-        this.interestRate=interestRate;
-    }
-    addInterest(){
-        if(this.balance>0){
-            const interest=(this.balance * this.interestRate)/100;
-            this.balance+=interest;
-        }
-
-    }
-}
-
-const user1=new Account("Abhinav",1000);
-const user2=new Account("Rahul",1000);
-const user3=new Account("Aniket",400);
-const user4=new Account("Yash",2500);
-const user5=new Account("Pritesh",4800);
-
-Account.showTotalAccounts();
-
-user2.deposit(500);      
-user2.addInterest();     
-user2.checkBalance();
